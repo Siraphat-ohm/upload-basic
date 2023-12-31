1c45708ce231612a4d87666dafd0922c6ade1a56
Siraphat-ohm <siraphat975@gmail.com>
2023-12-31 21:38:13 +0700

    readme

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-# Upload file API
-## How to use ?
+# Upload API
+
+## Upload a file to the server
+
+### Request
+`POST /upload`
+### Response
+`200 OK` on success
+
+## How to run ?
+
+### Install dependencies
+install dependencies using npm or yarn
+***USING NPM***
+```bash
+npm install
 ```
+***USING YARN***
+```bash
 yarn install
-yarn dev
+```
+
+### Run the server
+***USING NPM***
+```bash
+npm start
+```
+***USING YARN***
+```bash
+yarn start
 ```
--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,22 @@ import express, { Request, Response } from "express";
 import multer from "multer";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4662;
 const storage = multer.diskStorage({
     destination: function( req, file, callback ){
-        callback(null, './uploads')
+        callback(null, './uploads' );
     },
     filename: function ( req, file, callback ){
-        callback( null, file.originalname )
+        callback( null, file.originalname );
     }
-})
+});
 const upload = multer( { storage });
 
-app.post('/uploads', upload.single('file'), async ( req: Request, res:Response) => {
+app.post('/api/uploads', upload.single('file'), async ( req: Request, res:Response) => {
     console.log(req.file, req.body);
     res.sendStatus(200);
 });
 
 app.listen(port, () => {
-    console.log(`server start on port ${port}`);
-})
+    console.log(`Server is running on port ${port}`);
+});
